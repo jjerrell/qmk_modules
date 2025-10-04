2c43fc6b034a81605e6158241d110d5e4703d80b
Jacob Jerrell <86279411+jjerrell@users.noreply.github.com>
2025-10-04 07:50:38 -0500

    Explicitly reference shifted codes as they are not enumerated

--- shifted_mod_tap/shifted_mod_tap.c
+++ shifted_mod_tap/shifted_mod_tap.c
@@ -9,7 +9,27 @@ bool process_record_shifted_mod_tap(uint16_t keycode, keyrecord_t *record) {
     if (IS_QK_MOD_TAP(keycode) && record->tap.count) {
         keycode = QK_MOD_TAP_GET_TAP_KEYCODE(keycode);
         switch (keycode) {
-            case KC_TILDE ... KC_QUESTION:
+            case KC_TILDE:
+            case KC_EXCLAIM:
+            case KC_AT:
+            case KC_HASH:
+            case KC_DOLLAR:
+            case KC_PERCENT:
+            case KC_CIRCUMFLEX:
+            case KC_AMPERSAND:
+            case KC_ASTERISK:
+            case KC_LEFT_PAREN:
+            case KC_RIGHT_PAREN:
+            case KC_UNDERSCORE:
+            case KC_PLUS:
+            case KC_LEFT_CURLY_BRACE:
+            case KC_RIGHT_CURLY_BRACE:
+            case KC_PIPE:
+            case KC_COLON:
+            case KC_DOUBLE_QUOTE:
+            case KC_LEFT_ANGLE_BRACKET:
+            case KC_RIGHT_ANGLE_BRACKET:
+            case KC_QUESTION:
                 // Check tap.count to make sure we aren't processing a modifier
                 if (record->event.pressed) {
                     // Apply shift
